7aff1e0ce86c1a5f31d27b657e39fd1b970ef6ba
Rishabh <rishabh@arevei.com>
2025-10-25 14:28:28 +0530

    save

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager / Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BPC4B4QPPZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BPC4B4QPPZ');
+          `}
+        </Script>
+      </head>
       <body
         className={` font-poppins ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
